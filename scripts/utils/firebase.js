f635ca846a10fbe0b8bcbce9fb01b3649cc8b1ef
Tushar <tushardahiya84@gmail.com>
2020-05-20 16:33:15 +0530

    initial commit

---
+++ scripts/utils/firebase.js
@@ -0,0 +1,15 @@
+
+
+
+  // Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCtyqkL46tBzm-1qW9ygtSAfa6GIKGl5JI",
+  authDomain: "onlineshopping-74f5a.firebaseapp.com",
+  databaseURL: "https://onlineshopping-74f5a.firebaseio.com",
+  projectId: "onlineshopping-74f5a",
+  storageBucket: "onlineshopping-74f5a.appspot.com",
+  messagingSenderId: "88879901754",
+  appId: "1:88879901754:web:49ce5b2188f612f4"
+};
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
